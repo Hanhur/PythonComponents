fc1e5beef40507eee44b8f75e2bbd3d69343df53
Vladimír Hanhur <aqgz@email.cz>
2025-11-09 19:05:27 +0100

    update

--- CycleWhile/main.py
+++ CycleWhile/main.py
@@ -8,6 +8,16 @@
 try:
     apples = int(input("Введите количество яблок: "))
     people = int(input("Введите количество людей: "))
+
+    if apples < 0 or people < 0:
+        raise Exception
+
     print(f"Каждый человек должен получить по {apples / people} яблок")
 except ValueError:
     print("Введено неверное значение, попробуйте цифры")
+except ZeroDivisionError:
+    print("Людей не может быть 0!")
+except Exception:
+    print("Вы не можете использовать отрицательные числа!")
+finally:
+    print("Этот критический блок кода позади")
