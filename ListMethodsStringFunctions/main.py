89895c50d2de565783db529c8e6a5f627234050a
Vladimír Hanhur <aqgz@email.cz>
2026-04-10 19:44:58 +0200

    update

---
+++ ListMethodsStringFunctions/main.py
@@ -0,0 +1,22 @@
+import random
+
+# randint -> random integer
+# x = random.randint(1, 10)
+# count1 = 0
+# count2 = 0
+# for i in range(10000):
+#     x = random.random()
+#     if x >= 0.5:
+#         count1 += 1
+#     else:
+#         count2 += 1
+# print(count1, count2)
+
+# =========================================================
+
+# spisok = ['a', 'b', 'c', 'd', 'e', 'f']
+# spisok = ' '.join(spisok)
+# print(spisok[spisok.index('b') : spisok.index('b') + 1])
+
+# =========================================================
+
