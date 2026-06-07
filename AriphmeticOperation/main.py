bbda76b3fdc0a8a6879d8eab3b10b77db04f70f8
Vladimír Hanhur <aqgz@email.cz>
2026-06-07 16:44:59 +0200

    update

--- AriphmeticOperation/main.py
+++ AriphmeticOperation/main.py
@@ -1,4 +1,4 @@
-import pygame as pg, sys
+import pygame as pg, sys, random
 
 pg.init()
 screen = pg.display.set_mode((800, 600))
@@ -8,6 +8,7 @@ x, y = 400, 300
 size = 30 # Зазмет шарика
 color = (255, 0, 0) # Красный цвет. Зелёный - это (0, 255, 0).
 font = pg.font.SysFont("Arial", 24)
+shield = False
 
 # and (и) - соединяет 2 утверждения, так что истиной (тrue) будет. только если оба ут верждения верны;
 # or (или) - соединяет 2 утверждения, так что истиной (тrue) будет, если хотя бы одно из утверждений верно;
@@ -41,6 +42,12 @@ while True:
             sys.exit()
         elif event.type == pg.MOUSEBUTTONDOWN:
             x, y = pg.mouse.get_pos()
+            # Запрещаем выход за границы экрана
+            x = max(size, min(x, 800 - size)) # Ограничиваем x от 0 до 800
+            y = max(size, min(y, 600 - size)) # Ограничивам y от 0 до 600
+        elif event.type == pg.KEYDOWN:
+            if event.key == pg.K_s:
+                shield = not shield
 
     keys = pg.key.get_pressed()
     # Давайте сделаем так, чтобы можно быпо менять цвет нашен кисти.
@@ -56,9 +63,28 @@ while True:
     # Увеличение размера кисти
     if keys[pg.K_EQUALS] and size < 100: # И больше 100
         size += 1
+
     # Уменьшение размера кисти
     if keys[pg.K_MINUS] and size > 10: # Не дадим кружку стать меньше 10
         size -= 1
+
+    # До6авляем радужный режим
+    if keys[pg.K_c]:
+        color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+    # Проверяем, коснулся ли круг границ
+    touch_left = x - size <= 0 # Касается левой границы
+    touch_right = x + size >= 800 # Касается правой граицы
+    touch_top = y - size <= 0 # Касается верхией граицы
+    touch_bottom = y + size >= 600 # Касается нижней границы
+
+    if touch_left or touch_right or touch_top or touch_bottom:
+        color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+    if shield and (color == (255, 0, 0) or color == (0, 255, 0)):
+        # Если щит включён и цвет круга красный или зелёный
+        pg.draw.circle(screen, (0, 0, 255), (x, y), size + 10, 5)
+
     # Очищаем экран пробелом
     if keys[pg.K_SPACE]:
         screen.fill((255, 165, 0))
